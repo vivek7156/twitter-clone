7e92cd7e3d897e894c7df70bab9ba98ccd2309fc
“Vivek <viveksingh7156@gmail.com>
2024-10-13 11:46:45 +0530

    auth controller added

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use("/api/auth", authRoutes);
+app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
   connectMongoDB();
