3630616aa763d6029431cdd17afda77d79473526
“Vivek <viveksingh7156@gmail.com>
2024-10-14 17:56:46 +0530

    Post routes and controller added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express, { urlencoded } from "express";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -16,12 +17,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
   connectMongoDB();
